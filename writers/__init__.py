26fcfcc755a8f7ea8d3c15352a67c074f06b91ac
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-04-09 17:31:49 +0200

    0.3.5 automatically generated release

--- writers/__init__.py
+++ writers/__init__.py
@@ -18,7 +18,7 @@ from writers.verify import solve
 from writers.verify import validate
 from writers.verify import validate_template
 
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 PROCESS = 'writers'
