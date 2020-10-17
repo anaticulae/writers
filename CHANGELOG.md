76807a6186a445bc38b8c4cee5134f5094d99716
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-10-18 00:54:07 +0200

    0.4.2 automatically generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v0.4.2
+
 ## v0.4.1
 
 ## v0.4.0
--- writers/__init__.py
+++ writers/__init__.py
@@ -18,7 +18,7 @@ from writers.verify import solve
 from writers.verify import validate
 from writers.verify import validate_template
 
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 PROCESS = 'writers'
