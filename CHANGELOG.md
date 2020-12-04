629056cb031ddf188db341c06cc7964176b8f553
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-12-04 17:55:58 +0100

    1.1.3 automatically generated release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 Every noteable change is logged here.
 
+## v1.1.3
+
 ## v1.1.2
 
 ### Feature
--- writers/__init__.py
+++ writers/__init__.py
@@ -18,7 +18,7 @@ from writers.verify import solve
 from writers.verify import validate
 from writers.verify import validate_template
 
-__version__ = '1.1.2'
+__version__ = '1.1.3'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 PROCESS = 'writers'
